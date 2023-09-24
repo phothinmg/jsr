bc087c439b5a10cdad398ec0e30c76ec5fd5a3c2
phothinmg <phothinmg@gmail.com>
2023-09-24 12:24:29 +0630

    modified:   page-blog/pages.js

--- page-blog/pages.js
+++ page-blog/pages.js
@@ -1,26 +1,30 @@
 const home = `
-<h3>JSR </h3>
-<p class="hp">SPA Site Template (Javascript Router) </p>
+<h3>JSR <img src="/js.svg" alt="js"></h3>
+<p class="hp">SPA Template</p>
 <br>
 <hr>
+<a href = "https://github.com/phothinmg/jsr" class="ha" ><img src="/github.svg"></a>
 <br>
 <fieldset>
 <br>
 <legend>Resources</legend>
 <ul>
+<li><a href="https://simplecss.org/" target="_blank" class="ha" >Simple CSS</a></li>
+<br>
 <li><a href="https://github.com/iMykhailychenko/simple-router" target="_blank" class="ha" >Simple javascript router </a></li>
 <br>
-<li><a href="https://md-block.verou.me/" target="_blank" class="ha" > Markdown to Html MD-Block </a></li>
+<li><a href="https://md-block.verou.me/" target="_blank" class="ha" > Markdown to Html (MD-Block) </a></li>
 </ul>
 <br>
 </fieldset>
 <br><hr>
 <br>
 
-<a href = "https://github.com/phothinmg/jsr" class="ha" ><img src="/github.svg"> Git-Repo </a>
-<br><br><hr>
+<p class="fp">Generated with HTML , CSS , Javascript and Markdown.Power by <a class="fp" href="https://vitejs.dev/" target="_blank"> Vite</a>.  Host on <a class="fp" href="https://pages.cloudflare.com/" target="_blank">Cloudflare Pages</a>.</p>
+
+<br><hr>
 <br>
-<p class="fp">Generated with HTML , CSS , Javascript and Markdown. Power by Vite.</p>
+
 
 `;
 
